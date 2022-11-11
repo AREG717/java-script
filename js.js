7543cdbe0b30eca885a30d4b802c384e47815d60
AREG717 <ayvazyan.areg2015@mail.ru>
2022-11-12 00:24:57 +0400

    aaa

--- js.js
+++ js.js
@@ -1,8 +1,19 @@
+console.log("skizb1");
+
+let i=22;
+
+while(i<87){
+   i++;
+    console.log(i);
+}
+
+console.log("verj1");
+
+
 console.log("skizb2");
 
 console.log(2 ** 12);
 
-
-
+console.log("verj2");
 
 
